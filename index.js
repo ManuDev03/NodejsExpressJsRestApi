e624475ed5f698581dfda845c5c4ae5cc60ba4e9
ManuDev03 <manuprajan03@gmail.com>
2021-04-29 11:16:54 +0530

    req params object of blog

--- index.js
+++ index.js
@@ -17,8 +17,13 @@ app.get('/',(req,res)=>{
 app.get('/api/courses',(req,res)=>{
     res.send([1,2,3])
 })
+
 app.get('/api/courses/:id',(req,res)=>{
     res.send(req.params.id)
+})
+
+app.get('/api/posts/:year/:month',(req,res)=>{
+    res.send(req.params)
 })
 
 const port= process.env.PORT || 3000
