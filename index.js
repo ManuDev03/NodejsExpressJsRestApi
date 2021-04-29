c4ca22bad0bd8946ad4c387153ecbda0e236c5e9
ManuDev03 <manuprajan03@gmail.com>
2021-04-29 11:46:17 +0530

    handling http GET requests

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 
 // app.get()
 // app.post()
@@ -8,25 +14,21 @@ const app = express()
 
 // no if blocks
 
-
 //2 arg path,callback function-which will be called when we have an http request 2 arg req res
-app.get('/',(req,res)=>{
-    res.send('hello world ')
+app.get("/", (req, res) => {
+  res.send("hello world ");
+});
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
 
-})
-app.get('/api/courses',(req,res)=>{
-    res.send([1,2,3])
-})
+app.get("/api/courses/:id", (req, res) => {
+    const course =  courses.find(c => c.id === parseInt(req.params.id))
+    if(!course) res.status(404).send('the course with given id not found !!')
+    res.send(course)
+});
 
-app.get('/api/courses/:id',(req,res)=>{
-    res.send(req.params.id)
-})
-
-app.get('/api/posts/:year/:month',(req,res)=>{
-    res.send(req.params)
-})
-
-const port= process.env.PORT || 3000
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`);
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
