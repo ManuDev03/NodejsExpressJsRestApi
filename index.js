bd5bc55d8b86d2a92ff52770615817e4ed168af5
ManuDev03 <manuprajan03@gmail.com>
2021-04-29 12:09:20 +0530

    Handling Http POST requests

--- index.js
+++ index.js
@@ -23,9 +23,18 @@ app.get("/api/courses", (req, res) => {
 });
 
 app.get("/api/courses/:id", (req, res) => {
-    const course =  courses.find(c => c.id === parseInt(req.params.id))
-    if(!course) res.status(404).send('the course with given id not found !!')
-    res.send(course)
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("the course with given id not found !!");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
